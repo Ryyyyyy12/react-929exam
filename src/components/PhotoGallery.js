8dcb76743339b517de4e9cf044a86826c66ecfd7
Ryyyyyy12 <kevalin.sris@kmutt.ac.th>
2022-05-30 20:44:31 +0700

    Task4

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -3,6 +3,7 @@ import { MEMBERS } from '../shared/members'
 import Footer from './FooterComponent'
 import Header from './HeaderComponent'
 import {Input ,Button, Card, CardImg, CardText, CardBody, CardTitle , CardSubtitle, Form, FormGroup, Label, FormFeedback } from 'reactstrap';
+import { Modal , ModalBody , ModalHeader } from 'reactstrap';
 
 
 function Gallery() {
@@ -13,9 +14,19 @@ function Gallery() {
     const [comment,setComment] = useState("");
     const [isnameValid,setnameValid] = useState(false);
     const [ismentValid,setmentValid] = useState(false);
-    function clickLike(){
-        alert("you like this photo")
+    const [isModalOpen,setisModalOpen] = useState(false);
+    const [likes,setLikes] = useState([])
+    
+    function addLike(id,numlike){
+        var temp = likes.concat({id:selectedMems , numlike:1})
+        setLikes(temp)
+        alert("You like this photo , Click at the photo to see numbers of like");
     }
+    
+    function toggleModal(){
+        setisModalOpen(!isModalOpen);
+    }
+    
     function addComment(username,comment){
         var temp = userComment.concat({username , comment ,id:selectedMems})
         console.log(temp);
@@ -41,11 +52,24 @@ function Gallery() {
         }
         
     }
-    function renderSelected(members,selectedId){
+    function renderSelected(members,selectedId,likes){
         const printDes = members[selectedId]
-        // console.log(printDes)
+        const printLike = likes.filter(el=>el.id===selectedId)
+        console.log(printLike)
         if(selectedId!=null){
             return (
+                <div>
+                    <Modal isOpen={isModalOpen} toggle={toggleModal}>
+                    <ModalHeader toggle={toggleModal}>{printDes.name}</ModalHeader>
+                    <ModalBody>
+                        <img src={printDes.image} width="100%"></img>
+                        <div>
+                            <h4 align="center">number of likes : {printLike.length} </h4>
+                        </div>
+                        
+                    </ModalBody>
+                    </Modal>
+                
                 <div className='container mt-5'>
                     <Card style={{width:"100%"}}>
                         <div className='container mt-2'>
@@ -55,6 +79,7 @@ function Gallery() {
                             <p> <b> Description : </b>{printDes.description}</p>
                         </div>
                     </Card>
+                </div>
                 </div>
             )
         }else{
@@ -89,12 +114,13 @@ function Gallery() {
                     <div className='container'>
                         <div className='row'>
                             <div className='col'>
-                                <Card style={{width:"70%"}} key={el.id} onClick={()=>selectMember(el.id)}>
+                                <Card style={{width:"70%"}} key={el.id} onClick={()=>{selectMember(el.id);}}>
                                     {/* {console.log(selectedMems)} */}
                                     <CardImg
                                     src={el.image}
                                     top
                                     width="100%"
+                                    onClick={()=>toggleModal()}
                                     />
                                     <CardBody>
                                     <CardTitle tag="h5">
@@ -129,16 +155,16 @@ function Gallery() {
                                         <b>give score</b>
                                         <Form>
                                             <Input type="select" name="select" id="exampleSelect">
-                                            <option>1</option>
-                                            <option>2</option>
-                                            <option>3</option>
-                                            <option>4</option>
-                                            <option>5</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
                                             </Input>  
                                         </Form>
                                         </div>
                                         <Button color="success"onClick={()=>validate()}> Comment!</Button>
-                                        <Button color="primary"onClick={()=>clickLike()}>Like</Button>
+                                        <Button color="primary"onClick={()=>addLike(selectedMems,likes)}>Like</Button>
                                     </CardBody>
                                 </Card> 
                             </div>
@@ -169,7 +195,7 @@ function Gallery() {
                 <div className='col'>
                     <div className='container'>
                         <div className='row'>
-                            {renderSelected(members,selectedMems)}
+                            {renderSelected(members,selectedMems,likes)}
                         </div>
                         <div className='row mt-5'>
                             <Card style={{width:"100%" ,border:"0px",borderBlockColor:"lightblue"}}>
